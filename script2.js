7de118bb3a828bdfaa1f38e190bd0b688fed1e2c
TaisiaBodnar <39711110+TaisiaBodnar@users.noreply.github.com>
2018-09-05 18:46:24 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,37 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+ 
+function f5()
+{
+$('tx1').value="ПРИВЕТ,МИР";
+alert("ПРИВЕТ,МИР");
+}
+function f1(){
+	
+	$('x1').style.color='red';
+	
+}
+function f2(){
+	
+	$('x1').style.color='blue';
+	
+}
+function f3(){
+	
+	$('x1').style.color='green';
+	
+}
+function f4(){
+	
+	$('x1').style.color='purple';
+	
+}

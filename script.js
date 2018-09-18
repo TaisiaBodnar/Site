dc76ccc11cfc102381610698572d8ff8ca66e83f
TaisiaBodnar <39711110+TaisiaBodnar@users.noreply.github.com>
2018-09-19 01:41:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1(){
+	a=parseFloat($('tx1').value);
+	
+	if(a>0)
+	{ x1=a+1;
+	$('tx4').value=''+x1; }
+else 
+if(x1<=0)
+{$('tx4').value='Немає відповіді';}
+		}

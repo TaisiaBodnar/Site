8630a6bedae56ec71b536285322a2d9a9a472355
TaisiaBodnar <39711110+TaisiaBodnar@users.noreply.github.com>
2018-10-02 16:54:36 +0300

    Add files via upload

---
+++ script3cycle.js
@@ -0,0 +1,23 @@
+﻿﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1()
+{
+	A=parseFloat($('tx1').value);
+	B=parseFloat($('tx2').value);
+	razn=A;
+	while( razn>B)
+	{
+		razn=razn-B;
+	}			
+	$('tx4').value=razn;
+}

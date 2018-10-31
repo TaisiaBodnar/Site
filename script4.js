1d3e2ee62ef58552c638486993d44ec8a609b96d
TaisiaBodnar <39711110+TaisiaBodnar@users.noreply.github.com>
2018-10-31 12:21:45 +0200

    Update script4.js

--- script4.js
+++ script4.js
@@ -13,7 +13,7 @@ return elements; }
 function f(x)
 	{
 		var d = Date.parse(x.replace( /[.]/g,"-"));	
-		var mass = d.split('.');
+		var mass = x.split('.');
 		if( isNaN(d) )
 			{
       		 return false;
